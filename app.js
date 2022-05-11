3c2987eb9696827004f906bb8a8da1608f589c2d
k0zyX <barisdogan2k@gmail.com>
2022-05-11 22:48:14 +0300

    /about and /add_post routes added

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const { path } = require('express/lib/application');
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.set("view engine","ejs");
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
 });
 
 app.listen(port, () => {
